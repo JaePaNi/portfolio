0b2677e32e89355ac1c7e3aa8ce6bf6ea21a3753
jaepani <dev_jhkim@kakao.com>
2020-12-02 17:45:25 +0900

    javascript scroll opacity

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.addEventListener('scroll', () => {
     } else navbar.classList.remove('navbar--dark');
 });
 
-// navbar 메뉴선택시 이동
+// navbar, button 선택시 이동
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -26,4 +26,13 @@ const homeContact = document.querySelector('.home__contact');
 homeContact.addEventListener('click', () => {
     const element = document.querySelector('#contact');
     element.scrollIntoView({ behavior: "smooth" });
+});
+
+// scroll 할때 투명도 증가
+const homeContainer = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY, ':::', homeHeight)
+    homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
 });
