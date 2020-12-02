c4cc5f03da43d426b3ad1d90639d8b08f4d8b892
jaepani <dev_jhkim@kakao.com>
2020-12-02 17:20:42 +0900

    javascript navbar and btn scroll

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 'use strict'
 
 // navbar 스크롤링 했을 때 변화주기
-const navbar  = document.querySelector('#navbar');
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else navbar.classList.remove('navbar--dark');
+});
+
+// navbar 메뉴선택시 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link === null) {
+        return;
+    }
+    const element = document.querySelector(link);
+    element.scrollIntoView({ behavior: "smooth" });
+});
+
+const homeContact = document.querySelector('.home__contact');
+homeContact.addEventListener('click', () => {
+    const element = document.querySelector('#contact');
+    element.scrollIntoView({ behavior: "smooth" });
 });
