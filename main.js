c47973b77954cdfb8844cf8f411cd64c3763ff77
jaepani <dev_jhkim@kakao.com>
2020-12-03 13:36:49 +0900

    javascript work filtering

--- main.js
+++ main.js
@@ -33,6 +33,41 @@ const homeContainer = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY, ':::', homeHeight)
     homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// scroll시 arrow visible
+const arrow = document.querySelector('.arrow__up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrow.classList.add('visiable');
+    } else arrow.classList.remove('visiable');
+});
+
+// arrow버튼 클릭 시
+arrow.addEventListener('click', () => {
+    console.log('click')
+    const element = document.querySelector('#home');
+    element.scrollIntoView({ behavior: "smooth" });
+});
+
+// 개인프로젝트 부분 선택 카테고리
+const workContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects'); //animation effect 넣어주기 위해사용
+const projects = document.querySelectorAll('.project');
+workContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter === null) return 0;
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
 });
